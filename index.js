7b742a582f5443411ee5fdf680c8120c4a337409
Henri126 <118396736+Henri126@users.noreply.github.com>
2023-06-26 01:00:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var password = "YEA";
+
+function passcheck() {
+
+    if (document.getElementById('password').value !=password) {
+        alert('wrong password, try again.');
+        return false;
+    }
+
+ if (document.getElementById('password').value !=password) {
+        alert('correct password, yey.');
+    }
+
+}       
